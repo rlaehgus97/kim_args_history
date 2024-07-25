3a526129d5890b0782dc82d81f14deff85ca7cf8
rlaehgus97 <starkim625@gmail.com>
2024-07-25 15:19:00 +0900

    copy top10

---
+++ src/kim_args_history/db/utils.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def top():
+    df = pd.read_parquet('~/data/parquet')
+    fdf = df[df['dt'] == '2024-07-12']
+    sdf = fdf.sort_values(by='cnt', ascending=False).head(10)
+    ddf = sdf.drop(columns=['dt'])
+
+    r = ddf.to_string(index=False)
+    print(r)
