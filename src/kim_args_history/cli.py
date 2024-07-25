ea065192a056446411f1bca0d4381bcaa98c2de4
rlaehgus97 <starkim625@gmail.com>
2024-07-25 17:44:00 +0900

    cli.py complete

--- src/kim_args_history/cli.py
+++ src/kim_args_history/cli.py
@@ -14,24 +14,23 @@ def cmd():
                     description='What the program does',
                     epilog='Text at the bottom of help')
 
-    parser.add_argument('-s', '--scount')           
-    parser.add_argument('-t', '--top')      
-    parser.add_argument('-d', '--dt')  
+    parser.add_argument('-s', '--scount', type=str)           
+    parser.add_argument('-t', '--top', type=int)    
+    parser.add_argument('-d', '--dt', type=str)  
 
     args = parser.parse_args()
-    print(args.scount, args.top, args.dt)
 
     if args.scount:   #args.scount == True
-        print(f"-s => {args.scount}")
         # print command count
+        cnt = count(args.scount)
+        print(f"The command {args.scount} was used {cnt} times.")
+
     elif args.top:
-        print(f"-t => {args.top}")
         if args.dt:
-            print(f"-d => {args.dt}")
             # print command of specific date
+            print(top(args.top, args.dt))
         else:
-            parser.error("error: you must use top arg with  date argument")
-            #parser.print_help()
+            parser.error("error: you must use top arg with date argument")
 
     else:
         # print instruction
--- src/kim_args_history/db/utils.py
+++ src/kim_args_history/db/utils.py
@@ -1,14 +1,17 @@
 import pandas as pd
+#from tabulate import tabulate
 
 def read_data(path='~/data/parquet'):
     df = pd.read_parquet(path)
     return df
 
-def top(cnt, dt):
+def top(cnt=5, dt='2024-07-12'):
     df = read_data()
     fdf = df[df['dt'] == dt]
     sdf = fdf.sort_values(by='cnt', ascending=False).head(cnt)
-    ddf = sdf.drop(columns=['dt'])
+    
+    sdf = sdf.drop(columns=['ymd'])
+    ddf = sdf.drop(columns=['dt']).head(cnt)
 
     r = ddf.to_string(index=False)
     return r
